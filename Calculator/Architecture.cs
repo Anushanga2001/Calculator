dbd502e59307464e3afdbe0aae7f51042b5b1918
anushanga2001@gmail.com <anushanga2001@gmail.com>
2024-12-16 00:38:49 +0530

    add the architecture file

---
+++ Calculator/Architecture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Architecture
+    {
+        public Architecture() { }
+
+        public double Add(double num1, double num2)
+        {
+            // print hello world
+            Console.WriteLine("Hello World");
+        }
+    }
+}
