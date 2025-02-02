ba9358446dfbab3494b8e875aeda177028d47479
anushanga2001@gmail.com <anushanga2001@gmail.com>
2025-02-02 16:05:49 +0530

    done the changes

--- Calculator/Architecture.cs
+++ Calculator/Architecture.cs
@@ -9,11 +9,22 @@ namespace Calculator
     internal class Architecture
     {
         public Architecture() { }
-
-        public double Add(double num1, double num2)
+        
+        public double Add()
         {
             // print hello world
             Console.WriteLine("Hello World");
+            return 0.0;
+
+            // Let's perform a simple addition of two numbers
+            double num1 = 5.0;
+            double num2 = 10.0;
+            double result = num1 + num2;
+
+            // Print the result of the addition
+            Console.WriteLine($"The result of adding {num1} and {num2} is {result}");
+
+            return result;
         }
     }
 }
