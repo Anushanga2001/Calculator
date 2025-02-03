dbf465a2a6040739cf00f45a0503e8fb82e269da
anushanga2001@gmail.com <anushanga2001@gmail.com>
2025-02-03 21:38:17 +0530

    Fixed issues in Architecture.cs and added basic arithmetic operations

--- Calculator/Architecture.cs
+++ Calculator/Architecture.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,12 +10,14 @@ namespace Calculator
     internal class Architecture
     {
         public Architecture() { }
-        
+
         public double Add()
         {
             // print hello world
             Console.WriteLine("Hello World");
-            return 0.0;
+
+            // create the start pattern
+            Console.WriteLine("Start pattern");
 
             // Let's perform a simple addition of two numbers
             double num1 = 5.0;
@@ -26,5 +29,18 @@ namespace Calculator
 
             return result;
         }
+
+        public double Subtract()
+        {
+            // Let's perform a simple subtraction of two numbers
+            double num1 = 10.0;
+            double num2 = 5.0;
+            double result = num1 - num2;
+
+            // Print the result of the subtraction
+            Console.WriteLine($"The result of subtracting {num2} from {num1} is {result}");
+            return result;
+        }
+    }
     }
 }
